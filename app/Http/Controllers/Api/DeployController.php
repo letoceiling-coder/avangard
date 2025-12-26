d19264afccbdb030e78c4a3b940e281c392c3816
letoceiling-coder <dsc-23@yandex.ru>
2025-12-27 00:49:40 +0300

    Test deploy - РїСЂРѕРІРµСЂРєР° Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРѕРіРѕ РґРµРїР»РѕСЏ

--- app/Http/Controllers/Api/DeployController.php
+++ app/Http/Controllers/Api/DeployController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+// Test deploy - проверка автоматического деплоя через GitHub Actions
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
